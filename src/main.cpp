00aa7dd065740cfc2c0ce325dd8a87277e14462e
Ben Winding <ben.winding@gmail.com>
2018-05-03 14:09:53 +0930

    Fixed compilation error

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include "glm/gtx/string_cast.hpp"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
